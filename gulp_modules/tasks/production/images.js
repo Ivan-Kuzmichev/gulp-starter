d16bfd0407e62307015191bd341e4499501a46f8
Ivan Kuzmichev <ivan_kuzmichev@list.ru>
2019-09-01 13:08:45 +0300

    add task build images

--- gulp_modules/tasks/production/images.js
+++ gulp_modules/tasks/production/images.js
@@ -2,18 +2,50 @@ const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')({pattern: ['*', '!@*']});
 
 module.exports = function (options){
-    console.log(plugins)
     return function(callback){
-        return gulp.src(options.src)
+
+        var configSrcSet = plugins.responsiveConfig(
+            [
+                'app/styles/**/*.scss',
+                'app/templates/**/*.html'
+            ], {
+                extensions: [
+                    'jpg',
+                    'jpeg',
+                    'png',
+                    'webp'
+                ]
+            }
+        );
+        
+        return plugins.merge2(
+               gulp.src(options.src)
+                   .pipe(plugins.webp()),
+               gulp.src(options.src)
+            )
+            .pipe(plugins.responsive(configSrcSet, {
+                errorOnEnlargement: false,
+                withMetadata: false
+            }))
             .pipe(plugins.imagemin([
-                plugins.imagemin.gifsicle({interlaced: true}),
+                plugins.imageminGifsicle({interlaced: true}),
                 plugins.imageminJpegRecompress({
                     progressive: true,
                     max: 80,
                     min: 70
                 }),
                 plugins.imageminPngquant({quality: '80'}),
-                plugins.imagemin.svgo({plugins: [{removeViewBox: true}]})
+                plugins.imageminSvgo({plugins: [
+                    { removeViewBox: false },
+                    { removeUnusedNS: false },
+                    { removeUselessStrokeAndFill: false },
+                    { cleanupIDs: false },
+                    { removeComments: true },
+                    { removeEmptyAttrs: true },
+                    { removeEmptyText: true },
+                    { collapseGroups: true }
+                ]}),
+                plugins.imageminWebp({quality: 80})
             ]))
             .pipe(gulp.dest(options.dest))
         callback();
